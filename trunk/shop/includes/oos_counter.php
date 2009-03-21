64e34ed6c1f5b1084c0fdd5eee1e66a382a5dac3
r23 <r23@b10ad6e1-9362-0410-934c-b5a60136dca1>
2009-03-21 00:05:56 +0000

    misc update

--- trunk/shop/includes/oos_counter.php
+++ trunk/shop/includes/oos_counter.php
@@ -5,11 +5,11 @@
    OOS [OSIS Online Shop]
    http://www.oos-shop.de/
 
-   Copyright (c) 2003 - 2007 by the OOS Development Team.
+   Copyright (c) 2003 - 2009 by the OOS Development Team.
    ----------------------------------------------------------------------
    Based on:
 
-   File: counter.php,v 1.5 2003/02/10 22:30:52 hpdl 
+   File: counter.php,v 1.5 2003/02/10 22:30:52 hpdl
    ----------------------------------------------------------------------
    osCommerce, Open Source E-Commerce Solutions
    http://www.oscommerce.com
@@ -19,15 +19,16 @@
    Released under the GNU General Public License
    ---------------------------------------------------------------------- */
 
-  /** ensure this file is being included by a parent file */
-  defined( 'OOS_VALID_MOD' ) or die( 'Direct Access to this location is not allowed.' );
+/** ensure this file is being included by a parent file */
+defined( 'OOS_VALID_MOD' ) or die( 'Direct Access to this location is not allowed.' );
 
 //counter
-  $countertable = $oostable['counter'];
-  $query = "SELECT startdate, counter
-            FROM $countertable";
-  $counter_result =& $dbconn->Execute($query);
-  if (!$counter_result->RecordCount()) {
+$countertable = $oostable['counter'];
+$query = "SELECT startdate, counter
+          FROM $countertable";
+$counter_result =& $dbconn->Execute($query);
+
+if (!$counter_result->RecordCount()) {
     $date_now = date('Ymd');
     $counter_now = 1;
     $countertable = $oostable['counter'];
@@ -37,7 +38,7 @@
                                  . $dbconn->qstr($counter_now) . ")";
     $dbconn->Execute($query);
     $counter_startdate = $date_now;
-  } else {
+} else {
     $counter = $counter_result->fields;
     $counter_result->Close();
 
@@ -48,15 +49,15 @@
     $query = "UPDATE $countertable"
         . " SET counter = ?";
     $result =& $dbconn->Execute($query, array((int)$counter_now));
-  }
+}
 
-  $counter_startdate_formatted = strftime(DATE_FORMAT_LONG, mktime(0, 0, 0, substr($counter_startdate, 4, 2), substr($counter_startdate, -2), substr($counter_startdate, 0, 4)));
+$counter_startdate_formatted = strftime(DATE_FORMAT_LONG, mktime(0, 0, 0, substr($counter_startdate, 4, 2), substr($counter_startdate, -2), substr($counter_startdate, 0, 4)));
 
-  $oSmarty->assign(
+$oSmarty->assign(
       array(
           'counter_now' => $counter_now,
           'counter_startdate_formatted' => $counter_startdate_formatted
       )
-  );
+);
 
 ?>
