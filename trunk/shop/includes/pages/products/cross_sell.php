482f9484a87e67bb5b8162d644a2658e00e87dff
r23 <r23@b10ad6e1-9362-0410-934c-b5a60136dca1>
2009-03-23 00:55:59 +0000

    misc update

--- trunk/shop/includes/pages/products/cross_sell.php
+++ trunk/shop/includes/pages/products/cross_sell.php
@@ -5,35 +5,35 @@
    OOS [OSIS Online Shop]
    http://www.oos-shop.de/
 
-   Copyright (c) 2003 - 2007 by the OOS Development Team.
+   Copyright (c) 2003 - 2009 by the OOS Development Team.
    ----------------------------------------------------------------------
    Released under the GNU General Public License
    ---------------------------------------------------------------------- */
 
-  /** ensure this file is being required by a parent file */
-  defined( 'OOS_VALID_MOD' ) or die( 'Direct Access to this location is not allowed.' );
+/** ensure this file is being required by a parent file */
+defined( 'OOS_VALID_MOD' ) or die( 'Direct Access to this location is not allowed.' );
 
-  if (isset($_GET['products_id'])) {
+if (isset($_GET['products_id'])) {
     if (!isset($nProductsId)) $nProductsId = oos_get_product_id($_GET['products_id']);
-  } else {
+} else {
     oos_redirect(oos_href_link($aModules['main'], $aFilename['main']));
-  }
+}
 
-  require 'includes/languages/' . $sLanguage . '/products_cross_sell.php';
+require 'includes/languages/' . $sLanguage . '/products_cross_sell.php';
 
-  $productstable = $oostable['products'];
-  $products_descriptiontable = $oostable['products_description'];
-  $product_info_sql = "SELECT p.products_id, pd.products_name, p.products_model
-                        FROM $productstable p,
-                             $products_descriptiontable pd
-                        WHERE p.products_status >= '1'
-                          AND (p.products_access = '0' OR p.products_access = '" . intval($nGroupID) . "')
-                          AND p.products_id = '" . intval($nProductsId) . "'
-                          AND pd.products_id = p.products_id
-                          AND pd.products_languages_id = '" . intval($nLanguageID) . "'";
-  $product_info_result = $dbconn->Execute($product_info_sql);
+$productstable = $oostable['products'];
+$products_descriptiontable = $oostable['products_description'];
+$product_info_sql = "SELECT p.products_id, pd.products_name, p.products_model
+                      FROM $productstable p,
+                           $products_descriptiontable pd
+                      WHERE p.products_status >= '1'
+                        AND (p.products_access = '0' OR p.products_access = '" . intval($nGroupID) . "')
+                        AND p.products_id = '" . intval($nProductsId) . "'
+                        AND pd.products_id = p.products_id
+                        AND pd.products_languages_id = '" . intval($nLanguageID) . "'";
+$product_info_result = $dbconn->Execute($product_info_sql);
 
-  if (!$product_info_result->RecordCount()) {
+if (!$product_info_result->RecordCount()) {
 
     // product not found
     $aLang['text_information'] = $aLang['text_product_not_found'];
@@ -41,13 +41,13 @@
     $aOption['template_main'] = $sTheme . '/system/info.html';
     $aOption['page_heading'] = $sTheme . '/heading/page_heading.html';
 
-    $nPageType =OOS_PAGE_TYPE_PRODUCTS;
+    $nPageType = OOS_PAGE_TYPE_PRODUCTS;
 
     require 'includes/oos_system.php';
     if (!isset($option)) {
-      require 'includes/info_message.php';
-      require 'includes/oos_blocks.php';
-      require 'includes/oos_counter.php';
+        require 'includes/info_message.php';
+        require 'includes/oos_blocks.php';
+        require 'includes/oos_counter.php';
     }
 
     // assign Smarty variables;
@@ -59,14 +59,14 @@
         )
     );
 
-  } else {
+} else {
     $product_info = $product_info_result->fields;
 
     // links breadcrumb
     if (SHOW_PRODUCTS_MODEL == 'true') {
-      $oBreadcrumb->add($product_info['products_model'], oos_href_link($aModules['products'], $aFilename['product_info'], 'categories=' . $categories . '&amp;products_id=' . $nProductsId));
+        $oBreadcrumb->add($product_info['products_model'], oos_href_link($aModules['products'], $aFilename['product_info'], 'categories=' . $categories . '&amp;products_id=' . $nProductsId));
     } else {
-      $oBreadcrumb->add($product_info['products_name'], oos_href_link($aModules['products'], $aFilename['product_info'], 'categories=' . $categories . '&amp;products_id=' . $nProductsId));
+        $oBreadcrumb->add($product_info['products_name'], oos_href_link($aModules['products'], $aFilename['product_info'], 'categories=' . $categories . '&amp;products_id=' . $nProductsId));
     }
 
     $aOption['template_main'] = $sTheme . '/products/cross_sell_products.html';
@@ -80,9 +80,9 @@
 
     require 'includes/oos_system.php';
     if (!isset($option)) {
-      require 'includes/info_message.php';
-      require 'includes/oos_blocks.php';
-      require 'includes/oos_counter.php';
+        require 'includes/info_message.php';
+        require 'includes/oos_blocks.php';
+        require 'includes/oos_counter.php';
     }
 
     $oSmarty->assign('oos_breadcrumb', $oBreadcrumb->trail(BREADCRUMB_SEPARATOR));
@@ -91,36 +91,37 @@
     require 'includes/modules/history_products.php';
 
     if ( (USE_CACHE == 'true') && (!SID) ) {
-      $oSmarty->caching = true;
+        $oSmarty->caching = true;
     }
 
     if (!$oSmarty->is_cached($aOption['xsell_products'], $oos_products_info_cache_id)) {
-      require 'includes/modules/xsell_products.php';
+        require 'includes/modules/xsell_products.php';
     }
     $oSmarty->assign('xsell_products', $oSmarty->fetch($aOption['xsell_products'], $oos_products_info_cache_id));
 
     if (!$oSmarty->is_cached($aOption['up_sell_products'], $oos_products_info_cache_id)) {
-      require 'includes/modules/up_sell_products.php';
+        require 'includes/modules/up_sell_products.php';
     }
     $oSmarty->assign('up_sell_products', $oSmarty->fetch($aOption['up_sell_products'], $oos_products_info_cache_id));
 
     if (!$oSmarty->is_cached($aOption['also_purchased_products'], $oos_products_info_cache_id)) {
-      require 'includes/modules/also_purchased_products.php';
-      $oSmarty->assign('oos_also_purchased_array', $aPurchased);
+        require 'includes/modules/also_purchased_products.php';
+        $oSmarty->assign('oos_also_purchased_array', $aPurchased);
     }
     $oSmarty->assign('also_purchased_products', $oSmarty->fetch($aOption['also_purchased_products'], $oos_products_info_cache_id));
 
     if (!$oSmarty->is_cached($aOption['featured'], $oos_modules_cache_id)) {
-      require 'includes/modules/featured.php';
+        require 'includes/modules/featured.php';
     }
     $oSmarty->assign('featured', $oSmarty->fetch($aOption['featured'], $oos_modules_cache_id));
 
     $oSmarty->caching = false;
-  }
+}
 
-  $oSmarty->assign('oosPageHeading', $oSmarty->fetch($aOption['page_heading']));
-  $oSmarty->assign('contents', $oSmarty->fetch($aOption['template_main']));
+$oSmarty->assign('oosPageHeading', $oSmarty->fetch($aOption['page_heading']));
+$oSmarty->assign('contents', $oSmarty->fetch($aOption['template_main']));
 
-  // display the template
-  require 'includes/oos_display.php';
+// display the template
+require 'includes/oos_display.php';
+
 ?>
