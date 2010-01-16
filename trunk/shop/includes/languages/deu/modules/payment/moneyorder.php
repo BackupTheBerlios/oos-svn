e634205a39f8a62ac87c953164e55b3aa105afca
r23 <r23@b10ad6e1-9362-0410-934c-b5a60136dca1>
2010-01-16 23:28:39 +0000

    misc update

--- trunk/shop/includes/languages/deu/modules/payment/moneyorder.php
+++ trunk/shop/includes/languages/deu/modules/payment/moneyorder.php
@@ -5,7 +5,7 @@
    OOS [OSIS Online Shop]
    http://www.oos-shop.de/
 
-   Copyright (c) 2003 - 2009 by the OOS Development Team.
+   Copyright (c) 2003 - 2010 by the OOS Development Team.
    ----------------------------------------------------------------------
    Based on:
 
@@ -19,8 +19,8 @@
    Released under the GNU General Public License
    ---------------------------------------------------------------------- */
 
-define('MODULE_PAYMENT_MONEYORDER_STATUS_TITLE', 'Scheck/Vorkasse Modul aktivieren');
-define('MODULE_PAYMENT_MONEYORDER_STATUS_DESC', 'M&ouml;chten Sie die Bezahlung per Scheck/Vorkasse akzeptieren?');
+define('MODULE_PAYMENT_MONEYORDER_STATUS_TITLE', 'Bank&uuml;berweisung / Vorkasse Modul aktivieren');
+define('MODULE_PAYMENT_MONEYORDER_STATUS_DESC', 'M&ouml;chten Sie die Bezahlung per Bank&uuml;berweisung / Vorkasse akzeptieren?');
 
 define('MODULE_PAYMENT_MONEYORDER_PAYTO_TITLE', 'Zahlbar an:');
 define('MODULE_PAYMENT_MONEYORDER_PAYTO_DESC', 'An wen sollen Zahlungen erfolgen?');
@@ -34,7 +34,7 @@ define('MODULE_PAYMENT_MONEYORDER_ZONE_DESC', 'Wenn eine Zone ausgew&auml;hlt is
 define('MODULE_PAYMENT_MONEYORDER_ORDER_STATUS_ID_TITLE', 'Bestellstatus festlegen');
 define('MODULE_PAYMENT_MONEYORDER_ORDER_STATUS_ID_DESC', 'Bestellungen, welche mit diesem Modul gemacht werden, auf diesen Status setzen.');
 
-$aLang['module_payment_moneyorder_text_title'] = 'Scheck/Vorkasse';
+$aLang['module_payment_moneyorder_text_title'] = 'Bank&uuml;berweisung / Vorkasse';
 $aLang['module_payment_moneyorder_text_description'] = 'Zahlbar an:&nbsp;' . MODULE_PAYMENT_MONEYORDER_PAYTO . '<br />Adressat:<br /><br />' . nl2br(STORE_NAME_ADDRESS) . '<br /><br />' . 'Die Ware wird ausgeliefert wenn der Betrag auf unserem Konto eingegangen ist.';
 $aLang['module_payment_moneyorder_text_email_footer'] = "Zahlbar an: ". MODULE_PAYMENT_MONEYORDER_PAYTO . "\n\nAdressat:\n" . STORE_NAME_ADDRESS . "\n\n" . 'Die Ware wird ausgeliefert wenn der Betrag auf unserem Konto eingegangen ist.';
 
